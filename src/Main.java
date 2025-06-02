6353b0b2fcd0b1964c0b3e6503de865a89d2a985
solpi2 <parkchini@gmail.com>
2025-06-02 10:27:56 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 10 , b = 3;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+        int difference = a - b;
+        System.out.println(difference);
+
+        int product = a * b;
+        System.out.println(product);
+
+        int quotient = a / b;
+        System.out.println(quotient);
+
+        int remainder = a % b;
+        System.out.println(remainder);
 
     }
 }
